cab23c0d13cb01b389e01b1771142f29068e699c
sram-daw <sramiloalonso@danielcastelao.org>
2023-01-24 17:52:48 +0100

    # Conflicts: #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         //A
         //B
+        //lider 1
+        //lider 2
+        //lider 3
         //Modificacion colaborador 1
         //Modificacion colaborador 2
         //Modificacion colaborador 3
